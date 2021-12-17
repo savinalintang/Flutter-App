7d6cb6ac854f1d2b8225809caa2021c412754f9f
savinalintang <savinalintang7600@gmail.com>
2021-12-18 00:02:01 +0700

    Flutter app

---
+++ lib/screens/login.dart
@@ -0,0 +1,63 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: Container(
+        color: Theme.of(context).primaryColorDark,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Card(
+              elevation: 8,
+              margin: EdgeInsets.only(left: 8, right: 8),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                      ),
+                    ),
+                    TextField(
+                      keyboardType: TextInputType.visiblePassword,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => print("Login clicked"),
+                      child: Text('Login'),
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(double.infinity, 36),
+                      ),
+                    ),
+                    InkWell(
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: Text('Register new user'),
+                      ),
+                      onTap: () {
+                        Navigator.pushNamed(context, '/register');
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
